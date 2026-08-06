49f9db09c51d6b810284f3693b7916b18c144b3d
spectre-v2 <leif.stechmann@gmail.com>
2026-08-06 17:58:23 +0200

    csv converter verbessert

--- csv_text_buffer.c
+++ csv_text_buffer.c
@@ -5,40 +5,25 @@
 #include <stdio.h>
 
 
-void csv_entry_create(can_message_object_t *can_message, csv_log_entry_t *new_csv_entry){
+void csv_save_entry(FIL *fatfs_file, can_message_object_t *can_message){
 
-    snprintf(
-        (*new_csv_entry).can_id, 
-        sizeof((*new_csv_entry).can_id),
-        "%u",
-        (unsigned int)(*can_message).can_message.SID
-    );
-    char *char_write_pos = (*new_csv_entry).payload;
-    for(uint8_t payload_index=0; payload_index<64; payload_index++){
-        snprintf(
-            char_write_pos,
-            3,  //zwei stellen hex plus delimiter
-            "%02X",
-            (unsigned int)(*can_message).can_message.can_payload[payload_index]
-        );
-        char_write_pos = char_write_pos +2;
-   }
+    char csv_log_entry[145];
+    char *write_pointer = &csv_log_entry;
+    uint8_t written = 0;
+    written = snprintf(write_pointer, sizeof(csv_log_entry), "%u,", (unsigned int)(*can_message).can_message.SID);
+    write_pointer = write_pointer + written;
+
+    for (uint8_t payload_index = 0; payload_index>64; payload_index++){
+        
+        snprintf(write_pointer, 3, "%02X", (unsigned int)(*can_message).can_message.can_payload[payload_index]);
+
+        write_pointer = write_pointer + 2;
+        
+    }
+
+    snprintf(write_pointer, 2, "\n");
+
+    UINT bytes_written;
+    f_write(fatfs_file, csv_log_entry, strlen(csv_log_entry),&bytes_written);
 }
-
-void csv_entry_save(FIL *fatfs_file, csv_log_entry_t *csv_log_entry){
-        uint8_t bytes_written;
-        char log_entry[142];
-
-        snprintf(
-            &log_entry,
-            sizeof(log_entry),
-            "%s,%s\n",
-            csv_log_entry->can_id,
-            csv_log_entry->payload
-        );
-        f_write(&fatfs_file, log_entry, strlen(log_entry), &bytes_written);
-
-}
-
-
 
--- csv_text_buffer.h
+++ csv_text_buffer.h
@@ -2,12 +2,4 @@
 #include "can_types.h"
 
 
-typedef struct {
-    char can_id[11];
-    char payload[129];
-
-}csv_log_entry_t;
-
-void csv_entry_create(can_message_object_t *can_message, csv_log_entry_t *csv_entry);
-
-
+void csv_save_entry(FIL *fatfs_file, can_message_object_t *can_message);
